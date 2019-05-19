f9ec938cca001b1931037f9eb72a0fe75decd29a
BadiGamer <48218842+BadiGamer@users.noreply.github.com>
2019-05-19 10:15:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,16 @@
-client.on('guildMemberAdd', member => {
-const wlc = member.guild.channels.find("name","chat")
-wlc.send(`***___Welcome To Dark Server___ :heart:***
-             ***Have A Fun***`)
-})
+client.on('ready',async () => {//Toxic Codes
+console.log("Starting..");//Toxic Codes
+let g = client.guilds.get("576401840518856704"); // id server
+let c = g.channels.get("576780380934504448");// id channel
+if(c.type === 'voice') {//Toxic Codes
+c.join();//Toxic Codes
+setInterval(() => {//Toxic Codes
+if(!g.me.voiceChannel) c.join();
+}, 1);//Toxic Codes
+} else {//Toxic Codes
+console.log('Failed To Join: \n The Channel Type isn "Listening."')
+}
+});
 
 
 
